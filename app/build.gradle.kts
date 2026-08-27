3f703950f4bb875bbdde486953286e3e081f9e36
mekromn <67240529+mekromn@users.noreply.github.com>
2026-08-27 12:06:34 -0700

    Compile against Android API 37 with built-in Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.mekromn.geckovoice"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.mekromn.geckovoice"
@@ -31,10 +30,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 
     packaging {
